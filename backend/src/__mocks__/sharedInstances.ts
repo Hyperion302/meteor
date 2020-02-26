b6a8614a5b85374f0b750de2a189e5082cf6c6bb
Joseph Shetaye <Hyperion302@gmail.com>
2020-02-25 18:39:35 -0600

    Redo testing suite
    Third time's the charm!  Removed test files from tsconfig exclude, since that was causing them to not be hit by the TS language server (and all sorts of other weird problems)
    
    Flattened the mocked Firestore class so I can have mock access to all of it's functions, not just doc and collection.
    
    More importantly, I figured out the problems with jest.mock . I must call it after a require, then because it hoists itself, I can access module members only seen in the mocked version (mockData for example)
    
    I started mocking other modules like ChannelDataService and VideoContentService, since they are separate units and this is a unit test.

--- backend/src/__mocks__/sharedInstances.ts
+++ backend/src/__mocks__/sharedInstances.ts
@@ -1,90 +1,56 @@
 // Mock Firestore
+export const mockDoc = jest.fn();
+export const mockCollection = jest.fn();
+export const mockGet = jest.fn();
+export const mockSet = jest.fn();
+export const mockWhere = jest.fn();
+export const mockUpdate = jest.fn();
+// Optionally overriden
+export const mockExists = jest.fn(() => true);
+// mockData should have it's implementation overriden by the unit test
+export const mockData = jest.fn();
 
-const firestoreDB: any = {
-    videos: {
-        '3d1afd2a-04a2-47f9-9c65-e34b6465b83a': {
-            id: '3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
-            author: 'FDJIVPG1xgXfXmm67ETETSn9MSe2',
-            channel: '716886dd-c107-4bd7-9060-a47b50f81689',
-            content: 'b5263a52-1c05-4ab7-813d-65b8866bacfd',
-            description: 'Test Video Description',
-            title: 'Test Video Name',
-            uploadDate: 1578009691,
-        },
-    },
-    channels: {
-        '716886dd-c107-4bd7-9060-a47b50f81689': {
-            id: '716886dd-c107-4bd7-9060-a47b50f81689',
-            name: 'Test Channel',
-            owner: 'FDJIVPG1xgXfXmm67ETETSn9MSe2',
-        },
-    },
-    content: {
-        'b5263a52-1c05-4ab7-813d-65b8866bacfd': {
-            id: 'b5263a52-1c05-4ab7-813d-65b8866bacfd',
-            assetID: 'SNW1q1R01PdIkf26Kn01DIKAgYtq2qgWRo',
-            playbackID: '1ZjsLIn0167NzZ02TGbbGEngvGbMCAA00sG',
-        },
-    },
-};
-
-class FakeFirestoreDocSnap {
-    name: string;
-    collection: string;
-    path: string;
-    exists: boolean;
-    constructor(path: string) {
-        this.path = path;
-        // Get name out of path (I know we don't have nested collections so this will do)
-        const pathElements: string[] = path.split('/');
-        this.name = pathElements[1];
-        this.collection = pathElements[0];
-        if (
-            Object.keys(firestoreDB).includes(this.collection) && // Collection exists
-            Object.keys(firestoreDB[this.collection]).includes(this.name) // Doc exists in collection
-        ) {
-            this.exists = true;
-        } else {
-            this.exists = false;
-        }
-    }
-    data = jest.fn(() => {
-        return firestoreDB[this.collection][this.name];
-    });
-}
-
-class FakeFirestoreDoc {
-    path: string;
-    constructor(path: string) {
-        this.path = path;
-    }
-    public get = jest.fn(() => {
-        return new Promise((resolve, reject) => {
-            resolve(new FakeFirestoreDocSnap(this.path));
-        });
-    });
-}
-
-class FakeFirestoreQuerySnap {}
-
-class FakeFirestoreCollection {
-    public where = jest.fn(() => {
+export class MockedFirestore {
+    doc() {
+        mockDoc(...arguments);
         return this;
-    });
-    public get = jest.fn(() => {
-        return new Promise((resolve, reject) => {
-            resolve(new FakeFirestoreQuerySnap());
+    }
+    collection() {
+        mockCollection(...arguments);
+        return this;
+    }
+    get() {
+        mockGet(...arguments);
+        return new Promise((resolve) => {
+            process.nextTick(() => {
+                resolve(this);
+            });
         });
-    });
+    }
+    get exists() {
+        return mockExists();
+    }
+    public data = mockData;
+    set() {
+        mockSet(...arguments);
+        return new Promise((resolve) => {
+            process.nextTick(() => {
+                resolve();
+            });
+        });
+    }
+    update() {
+        mockUpdate(...arguments);
+        return new Promise((resolve) => {
+            process.nextTick(() => {
+                resolve();
+            });
+        });
+    }
+    where() {
+        mockWhere(...arguments);
+        return this;
+    }
 }
 
-class FakeFirestore {
-    public doc = jest.fn((path: string) => {
-        return new FakeFirestoreDoc(path);
-    });
-    public collection = jest.fn(() => {
-        return new FakeFirestoreCollection();
-    });
-}
-
-export const firestoreInstance = new FakeFirestore();
+export const firestoreInstance = new MockedFirestore();
--- backend/src/services/VideoDataService/index.spec.ts
+++ backend/src/services/VideoDataService/index.spec.ts
@@ -1,56 +1,113 @@
 import * as videoDataService from './';
-import { firestoreInstance } from '../../sharedInstances';
-
+const sharedInstances = require('../../sharedInstances');
+const channelDataService = require('../ChannelDataService');
+const videoContentService = require('../VideoContentService');
 jest.mock('../../sharedInstances');
+jest.mock('../ChannelDataService');
+jest.mock('../VideoContentService');
+
+function mockImplementations() {
+    // Mock firestore document
+    sharedInstances.mockData.mockImplementation(() => {
+        return {
+            id: '3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
+            author: 'FDJIVPG1xgXfXmm67ETETSn9MSe2',
+            channel: '716886dd-c107-4bd7-9060-a47b50f81689',
+            content: 'b5263a52-1c05-4ab7-813d-65b8866bacfd',
+            description: 'Test Video Description',
+            title: 'Test Video Name',
+            uploadDate: 1578009691,
+        };
+    });
+    // Mock Channel
+    channelDataService.getChannel.mockImplementation((id: string) => {
+        return new Promise((resolve) => {
+            process.nextTick(() => {
+                resolve({
+                    id: '716886dd-c107-4bd7-9060-a47b50f81689',
+                    name: 'Test Channel',
+                    owner: 'FDJIVPG1xgXfXmm67ETETSn9MSe2',
+                });
+            });
+        });
+    });
+    // Mock Content
+    videoContentService.getVideo.mockImplementation((id: string) => {
+        return new Promise((resolve) => {
+            process.nextTick(() => {
+                resolve({
+                    id: 'b5263a52-1c05-4ab7-813d-65b8866bacfd',
+                    assetID: 'SNW1q1R01PdIkf26Kn01DIKAgYtq2qgWRo',
+                    playbackID: '1ZjsLIn0167NzZ02TGbbGEngvGbMCAA00sG',
+                });
+            });
+        });
+    });
+}
+
+beforeAll(() => {
+    mockImplementations();
+});
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
 
 describe('Video Data Service', () => {
     describe('getVideo', () => {
-        it('requests the correct video ID', async () => {
+        it('Requests the correct video ID', async () => {
             await videoDataService.getVideo(
                 '3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
             );
-            expect(firestoreInstance.doc).toBeCalledWith(
+            expect(sharedInstances.mockDoc).toBeCalledWith(
                 'videos/3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
             );
         });
-        it('requests the correct channel ID', async () => {
+        it('Checks if the video exists', async () => {
             await videoDataService.getVideo(
                 '3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
             );
-            expect(firestoreInstance.doc).toBeCalledWith(
-                'channels/716886dd-c107-4bd7-9060-a47b50f81689',
-            );
+            expect(sharedInstances.mockExists).toBeCalled();
         });
-        it('requests the correct content ID', async () => {
+        it('Requests the correct channel ID', async () => {
             await videoDataService.getVideo(
                 '3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
             );
-            expect(firestoreInstance.doc).toBeCalledWith(
-                'content/b5263a52-1c05-4ab7-813d-65b8866bacfd',
+            expect(channelDataService.getChannel).toBeCalledWith(
+                '716886dd-c107-4bd7-9060-a47b50f81689',
             );
         });
-        it('responds with the correct video', async () => {
+        it('Requests the correct content ID', async () => {
+            await videoDataService.getVideo(
+                '3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
+            );
+            expect(videoContentService.getVideo).toBeCalledWith(
+                'b5263a52-1c05-4ab7-813d-65b8866bacfd',
+            );
+        });
+        it('Responds with the correct video', async () => {
             const res = await videoDataService.getVideo(
                 '3d1afd2a-04a2-47f9-9c65-e34b6465b83a',
             );
-            expect(res.author).toBe('FDJIVPG1xgXfXmm67ETETSn9MSe2');
-            expect(res.description).toBe('Test Video Description');
-            expect(res.title).toBe('Test Video Name');
-            expect(res.id).toBe('3d1afd2a-04a2-47f9-9c65-e34b6465b83a');
-            expect(res.uploadDate).toBe(1578009691);
-            // Channel
-            expect(res.channel.id).toBe('716886dd-c107-4bd7-9060-a47b50f81689');
-            expect(res.channel.name).toBe('Test Channel');
-            expect(res.channel.owner).toBe('FDJIVPG1xgXfXmm67ETETSn9MSe2');
-            // Content
-            expect(res.content.id).toBe('b5263a52-1c05-4ab7-813d-65b8866bacfd');
-            expect(res.content.assetID).toBe(
-                'SNW1q1R01PdIkf26Kn01DIKAgYtq2qgWRo',
-            );
-            expect(res.content.playbackID).toBe(
-                '1ZjsLIn0167NzZ02TGbbGEngvGbMCAA00sG',
-            );
+            expect(res).toMatchInlineSnapshot(`
+                Object {
+                  "author": "FDJIVPG1xgXfXmm67ETETSn9MSe2",
+                  "channel": Object {
+                    "id": "716886dd-c107-4bd7-9060-a47b50f81689",
+                    "name": "Test Channel",
+                    "owner": "FDJIVPG1xgXfXmm67ETETSn9MSe2",
+                  },
+                  "content": Object {
+                    "assetID": "SNW1q1R01PdIkf26Kn01DIKAgYtq2qgWRo",
+                    "id": "b5263a52-1c05-4ab7-813d-65b8866bacfd",
+                    "playbackID": "1ZjsLIn0167NzZ02TGbbGEngvGbMCAA00sG",
+                  },
+                  "description": "Test Video Description",
+                  "id": "3d1afd2a-04a2-47f9-9c65-e34b6465b83a",
+                  "title": "Test Video Name",
+                  "uploadDate": 1578009691,
+                }
+            `);
         });
     });
-    describe('queryVideo', () => {});
 });
