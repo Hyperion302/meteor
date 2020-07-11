de06a3e3d18204dfe5cfa060adeef82f35151d9b
Joseph Shetaye <josephs@jtstechnic.com>
2020-07-11 11:40:38 -0500

    Make minor changes to watch page UI

--- web/swish/components/PlayerChannelTile.vue
+++ web/swish/components/PlayerChannelTile.vue
@@ -1,13 +1,11 @@
 <template>
   <nuxt-link tag="div" class="smallChannelTile" :to="`/channel/${channel.id}`">
-    <img
+    <wrapped-image
       class="channelIcon"
-      :src="iconURL"
-      @error="
-        () => {
-          iconExists = false;
-        }
+      :src="
+        `https://storage.googleapis.com/prod-swish/channelIcons/${channel.id}_64.png`
       "
+      :size="64"
     />
     <h2 class="channelName">{{ channel.name }}</h2>
   </nuxt-link>
@@ -15,20 +13,16 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator';
+import WrappedImage from '~/components/WrappedImage.vue';
 import { IChannel } from '~/models/channel';
 
-@Component({})
+@Component({
+  components: {
+    WrappedImage,
+  },
+})
 export default class PlayerChannelTile extends Vue {
-  iconExists: boolean = true;
   @Prop({ required: true }) readonly channel!: IChannel;
-
-  get iconURL() {
-    if (this.iconExists) {
-      return `https://storage.googleapis.com/prod-swish/channelIcons/${this.channel.id}_64.png`;
-    } else {
-      return 'https://via.placeholder.com/64';
-    }
-  }
 }
 </script>
 
@@ -42,15 +36,17 @@ export default class PlayerChannelTile extends Vue {
   align-items: center;
   border-radius: 5px;
   padding: 5px;
+  color: #666666;
   &:hover
-    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.25);
     cursor: pointer;
+    color: #333333;
   h2
     margin: 0 0 0 5px;
-    font-weight: 200;
+    font-weight: 400;
     font-size: 16px;
     text-overflow: ellipsis;
   img
     width: 60px;
     height: 60px;
+    border-radius: 50%;
 </style>
