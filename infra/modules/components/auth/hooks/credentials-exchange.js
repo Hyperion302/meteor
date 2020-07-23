1bf70cc87dd230edbbae073a463c8814ab5088ba
Joseph Shetaye <josephs@jtstechnic.com>
2020-07-23 09:04:36 -0500

    Hardcode the testing machine's ID

--- infra/modules/components/auth/hooks/credentials-exchange.js
+++ infra/modules/components/auth/hooks/credentials-exchange.js
@@ -1,5 +1,5 @@
 module.exports = function (client, scope, audience, context, cb) {
   const access_token = {};
-  access_token['https://swish.tv/swishflake'] = client.metadata.swishflake;
+  access_token['https://swish.tv/swishflake'] = '73946096308584448';
   cb(null, access_token);
 };
